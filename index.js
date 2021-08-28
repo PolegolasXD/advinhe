7c595213a46274755323d70ac666237002a6fd5c
4lysson-a <4ly.alcantara@gmail.com>
2021-08-28 00:07:23 -0300

    Adicionando Script

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("Salve Curiosos <3 ");
+var chute = document.querySelector(".chute");
+var tentativa = document.querySelector(".tentativa");
+var acertos = document.querySelector(".acertos");
+var palpite = document.querySelector(".campo-do-palpite");
+var resposta = document.querySelector(".resposta");
+console.log(resposta);
+var contagemPalpites 
+function conferirPalpites() {
+  console.log("Rapaz ;-; sei de nada")
+}
+conferirPalpites();
